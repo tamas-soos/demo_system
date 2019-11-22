503b35b76012c14c3986e281b1ec5465c70b44bd
Tamas Soos <soostamas.hu@gmail.com>
2019-11-22 00:45:24 +0000

    Update geometry.ex

--- tech_session/lib/tech_session/geometry.ex
+++ tech_session/lib/tech_session/geometry.ex
@@ -7,10 +7,6 @@ defmodule Geometry do
     a * a
   end
 
-  def area(:square, a) do
-    a * a
-  end
-
   def area({:circle, r}) do
     r * r * 3.14
   end
@@ -18,4 +14,17 @@ defmodule Geometry do
   def area(shape) do
     {:error, {:unknown_shape, shape}}
   end
+
+  # def area(:square, a) do
+  #   a * a
+  # end
+
+  # def area(shape) do
+  #   case shape do
+  #     {:rectangle, a, b} -> a * b
+  #     {:square, a} -> a * a
+  #     {:circle, r} -> r * r * 3.14
+  #     _ -> {:error, {:unknown_shape, shape}}
+  #   end
+  # end
 end
